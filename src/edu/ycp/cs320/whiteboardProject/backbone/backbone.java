9f1ee0f7d4dac7955674241a4fd00bbbe011240a
Aaron Walsh <awalsh6@ycp.edu>
2016-01-26 17:09:27 -0500

    Made an Applet
    Started an applet to print hello world. Used the init() method- one of
    many that can be overridden from the JApplet superclass. See the JApplet
    documentation.

--- src/edu/ycp/cs320/whiteboardProject/backbone/backbone.java
+++ src/edu/ycp/cs320/whiteboardProject/backbone/backbone.java
@@ -1,12 +1,25 @@
 package edu.ycp.cs320.whiteboardProject.backbone;
 
-public class backbone {
+import javax.swing.JApplet;
+
+public class backbone extends JApplet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L; //what is this shit?
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//TESTING WORK PLS
 		// caras comment
+		//testing branch lel
+		//System.out.println("Hello World");
+	}
+	
+	public void init() {
+		System.out.println("Hello World");
 	}
 
 }
